e02f43561f6392f40439a3e59f305a49ba931e89
Pallab Banerjee <91554568+pallab17@users.noreply.github.com>
2024-07-02 03:20:30 +0530

    Time: 2 ms (64.65%) | Memory: 10.6 MB (45.73%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/three-consecutive-odds.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
     bool threeConsecutiveOdds(vector<int>& arr) {
-         int p = arr.size();
-        for(int i=0;i<p-2;i++){
+         //int p = arr.size();
+          if (arr.size() < 3) {
+            // If the size of the array is less than 3, we cannot have three consecutive odds
+            return false;
+        }
+        for(int i=0;i<arr.size()-2;i++){
             if(arr[i] % 2 == 1 && arr[i+1] % 2 == 1 && arr[i+2] % 2 == 1){
                 return true;
             }
