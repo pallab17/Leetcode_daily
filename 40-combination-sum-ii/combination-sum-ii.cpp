0bf02adfb186f39dd2dbd50f930d5cce2f37f083
Pallab Banerjee <91554568+pallab17@users.noreply.github.com>
2024-08-13 21:24:31 +0530

    Time: 4 ms (60.85%) | Memory: 12.8 MB (58.11%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+ void solve(int angul,vector<int>& candidates,int target,vector<vector<int>>&res,vector<int>&baksho){
+    if(target==0){   // base case 
+        res.push_back(baksho);
+        return;
+    }
+    if(target<0){
+        return;
+    }
+    for(int i = angul; i< candidates.size();i++){
+        if(i>angul &&  candidates[i]==candidates[i-1]) continue;
+        baksho.push_back(candidates[i]);
+        solve(i+1,candidates,target-candidates[i],res,baksho);
+        baksho.pop_back();
+    }
+ }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>>res;
+        vector<int>baksho;
+        solve(0,candidates,target,res,baksho);
+        return res;   
+    }
+};
