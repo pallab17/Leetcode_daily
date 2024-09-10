db92ccee324d27fe1103480cbd16c5d86fdcd402
Pallab Banerjee <91554568+pallab17@users.noreply.github.com>
2024-09-10 22:20:33 +0530

    Time: 40 ms (54.53%) | Memory: 36 MB (7.93%) - LeetSync

--- 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.cpp
+++ 2903-insert-greatest-common-divisors-in-linked-list/insert-greatest-common-divisors-in-linked-list.cpp
@@ -11,27 +11,50 @@
 class Solution {
 public:
     ListNode* insertGreatestCommonDivisors(ListNode* head) {
+        // base case
         if(!head || !head->next) return head;
+// iterative soln Tc - O(N)  sc - (1)
+        // ListNode *currentNode = head;
+        // ListNode *nextNode = head->next;
 
-        ListNode *currentNode = head;
-        ListNode *nextNode = head->next;
+        // while(nextNode){  // nextNode jodi na ei thake tahole gcd ber korbo ki bhabe
 
-        while(nextNode){  // nextNode jodi na ei thake tahole gcd ber korbo ki bhabe
+        // ListNode *gcdNode = new ListNode(__gcd(currentNode->val,nextNode->val));
 
-        ListNode *gcdNode = new ListNode(__gcd(currentNode->val,nextNode->val));
+        // currentNode->next = gcdNode;
 
-        currentNode->next = gcdNode;
+        // gcdNode->next = nextNode;
 
-        gcdNode->next = nextNode;
+        // currentNode = nextNode;
 
-        currentNode = nextNode;
-
-        nextNode = nextNode->next;
+        // nextNode = nextNode->next;
 
 
 
-        }
+        // }
         
-        return head;
+        // return head;
+
+
+// recursive soln
+// tc - O(N)
+// sc - O(N) recursion stack space
+
+// recursion ke send korchi head r porer node gulo
+        ListNode *recursionNode = insertGreatestCommonDivisors(head->next);
+
+// gcd node ta bnaiye value push korchi
+ListNode *gcdNode = new ListNode(__gcd(head->val,head->next->val));
+
+
+gcdNode->next = recursionNode;
+
+head->next = gcdNode;
+
+
+return head;
+
+
+
     }
 };
