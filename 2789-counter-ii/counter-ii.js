2230fcb02456f554805e634450ab6e8611e96b57
Pallab Banerjee <91554568+pallab17@users.noreply.github.com>
2024-12-16 18:37:45 +0530

    Time: 49 ms (94.38%) | Memory: 51.5 MB (69.65%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,31 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+const n = init;
+    return obj ={
+        increment(){
+            return init += 1;
+
+        },
+        decrement(){
+            return init -= 1;
+        
+
+        },
+        reset(){
+            return init = n;
+
+
+        }
+    }
+    
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
