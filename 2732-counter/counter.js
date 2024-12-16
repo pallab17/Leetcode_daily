ed6517229d57515adb19acaa37867608c9256186
Pallab Banerjee <91554568+pallab17@users.noreply.github.com>
2024-12-16 17:52:46 +0530

    Time: 56 ms (25.48%) | Memory: 49.2 MB (18.47%) - LeetSync

--- 2732-counter/counter.js
+++ 2732-counter/counter.js
@@ -8,8 +8,14 @@
 //         return n++;
 //     };
 // };
+// var createCounter = function(n) {
+//     var count = n;
+//     return function() {
+//         return n++;
+//     };
+// };
 var createCounter = function(n) {
-    var count = n;
+    let count = n;
     return function() {
         return n++;
     };
