e9be23f1f6476d53d7423215422f191471729551
Pallab Banerjee <91554568+pallab17@users.noreply.github.com>
2025-01-01 13:40:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (38.26%) - LeetSync

---
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxScore(string s) {
+
+        int n= s.length();
+
+        int zc =0;
+        int oc=0;
+        int result = INT_MIN;
+
+        for(int i=0;i<=n-2;i++){
+            if(s[i]=='1'){
+                oc++;
+            }else{
+                zc++;
+            }
+            result = max(result,zc-oc);
+        }
+
+        if(s[n-1]=='1'){
+            oc++;
+        }
+
+        return result+oc;
+  
+        
+    }
+};
