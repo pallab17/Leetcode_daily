0e8479b39247ab5c22b050de18caf6eca22ef59b
Pallab Banerjee <91554568+pallab17@users.noreply.github.com>
2025-01-23 16:44:11 +0530

    Time: 0 ms (100.00%) | Memory: 26.6 MB (59.58%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     int countServers(vector<vector<int>>& grid) {
 
+// tc - O(row*col) + O(row*col) = O(row*col)
+// sc - O(row  + col )
+
         int noofrows = grid.size();
         int noofcols = grid[0].size();
 
@@ -19,6 +22,7 @@ public:
 
         int output=0;
 
+// 2nd time traversal r time e check korchi oi particular row ba column e 1 tar beshi server ache kina jodi thake tahole ans e ekta count barabo
         for(int row=0;row<noofrows;row++){
             for(int col=0;col<noofcols;col++){
                 if(grid[row][col]==1 && (rcs[row]>1 || ccs[col]>1) ){
