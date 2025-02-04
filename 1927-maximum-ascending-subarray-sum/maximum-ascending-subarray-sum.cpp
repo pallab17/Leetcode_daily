6564fb4d2b89c0257ed6f622ef42e40616e06546
Pallab Banerjee <91554568+pallab17@users.noreply.github.com>
2025-02-05 00:36:45 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (100.00%) - LeetSync

--- 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.cpp
+++ 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     int maxAscendingSum(vector<int>& nums) {
 
+        // tc - O(N)
+        // sc - O(1)
+
         int n = nums.size();
 
         int sum = nums[0];
@@ -10,15 +13,50 @@ public:
         for(int i =1;i<n;i++){
             if(nums[i]>nums[i-1]){
                 sum+= nums[i];
+                
             }else{
                 maxsum=max(maxsum,sum);
-                sum = nums[i];
+                sum = nums[i];    
             }
         }
 
         return max(sum,maxsum);
+        
 
 
         
     }
 };
+
+
+// Wrong Answer if the code is tweaked
+//  int n = nums.size();
+
+//         int sum = nums[0];
+//         int maxsum = 0;
+
+//         for(int i =1;i<n;i++){
+//             if(nums[i]>nums[i-1]){
+//                 sum+= nums[i];
+//                 maxsum=max(maxsum,sum);   
+//             }else{
+//                
+//                 sum = nums[i];    // 1
+//             }
+//         }
+
+//         
+//          return max(sum,maxsum);   1,0 ans =1
+
+// 97 / 104 testcases passed
+
+// Editorial
+// Input
+// nums =
+// [100,10,1]
+
+// Use Testcase
+// Output
+// 1
+// Expected
+// 100
