258d875d252dddaa1f9e847a639f5cbcd154ce5a
Pallab Banerjee <91554568+pallab17@users.noreply.github.com>
2025-02-10 16:35:10 +0530

    Time: 1 ms (25.26%) | Memory: 8.5 MB (70.42%) - LeetSync

--- 3447-clear-digits/clear-digits.cpp
+++ 3447-clear-digits/clear-digits.cpp
@@ -1,27 +1,48 @@
 class Solution {
 public:
     string clearDigits(string s) {
-        
-        stack<int>st;
 
-        for(char &ch:s){
-            if(isdigit(ch)){
-                if(st.empty()==NULL){
-                    st.pop();
+int i = 0;
+        while(i<s.length()){
+            if(isdigit(s[i])){
+                s.erase(i,1);
+
+                if(i-1>=0){
+                    s.erase(i-1,1);
+                    i--;
                 }
-            }else{
-                st.push(ch);
+            }else 
+            {
+                i++;
             }
+
+
         }
 
-        string result ="";
-        while(st.empty()==NULL){
-            result.push_back(st.top());
-            st.pop();
-        }
+        return s;
 
-        reverse(result.begin(),result.end());
+        
+        // tc - O(N) sc - O(N)
+        // stack<int>st;
 
-        return result;
+        // for(char &ch:s){
+        //     if(isdigit(ch)){
+        //         if(st.empty()==NULL){
+        //             st.pop();
+        //         }
+        //     }else{
+        //         st.push(ch);
+        //     }
+        // }
+
+        // string result ="";
+        // while(st.empty()==NULL){
+        //     result.push_back(st.top());
+        //     st.pop();
+        // }
+
+        // reverse(result.begin(),result.end());
+
+        // return result;
     }
 };
