56a1afc65e291c79fe0dad6f2cce0afd5818463a
Pallab Banerjee <91554568+pallab17@users.noreply.github.com>
2025-02-13 04:45:43 +0530

    Time: 63 ms (18.84%) | Memory: 75.2 MB (23.40%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -38,36 +38,70 @@
 //Approach-2 (Optimal array as Map of constant size)
 //T.C : O(n*m), m = number of digits
 //S.C : O(1)
+// class Solution {
+// public:
+//     int getDigitSum(int num) {
+//         int sum = 0;
+
+//         while(num > 0) {
+//             sum += (num % 10);
+
+//             num /= 10;
+//         }
+
+//         return sum;
+//     }
+
+//     int maximumSum(vector<int>& nums) {
+//         int n = nums.size();
+//         int result = -1;
+
+//         int mp[82] = {0};
+
+//         for(int i = 0; i < n; i++) {
+//             int digitSum = getDigitSum(nums[i]);
+
+//             if(mp[digitSum] > 0) {
+//                 result = max(result, nums[i] + mp[digitSum]);
+//             }
+
+//             mp[digitSum] = max(mp[digitSum], nums[i]);
+//         }
+
+//         return result;
+//     }
+// };
+
+
 class Solution {
 public:
-    int getDigitSum(int num) {
+    int maximumSum(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<pair<int,int>> parr;
+        int res = -1;
+        for(int i = 0; i < n;i ++){
+            int DigitSum = sumOfDigits(nums[i]);
+            parr.push_back({DigitSum, nums[i]});
+        }
+
+        sort(parr.begin(), parr.end());
+
+        for(int i = 1; i < n; i++){
+            if(parr[i].first == parr[i-1].first){
+                res = max(res, parr[i].second + parr[i-1].second);
+            }
+        }
+        return res;
+    }
+
+    int sumOfDigits(int n){
         int sum = 0;
-
-        while(num > 0) {
-            sum += (num % 10);
-
-            num /= 10;
+        while(n > 0){
+            sum += (n % 10);
+            n = n / 10;
         }
 
         return sum;
-    }
-
-    int maximumSum(vector<int>& nums) {
-        int n = nums.size();
-        int result = -1;
-
-        int mp[82] = {0};
-
-        for(int i = 0; i < n; i++) {
-            int digitSum = getDigitSum(nums[i]);
-
-            if(mp[digitSum] > 0) {
-                result = max(result, nums[i] + mp[digitSum]);
-            }
-
-            mp[digitSum] = max(mp[digitSum], nums[i]);
-        }
-
-        return result;
     }
 };
