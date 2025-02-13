bbf872c9177eb88cfb1eef82c52d1e6b598db2e0
Pallab Banerjee <91554568+pallab17@users.noreply.github.com>
2025-02-14 01:26:44 +0530

    Time: 96 ms (59.31%) | Memory: 91.3 MB (90.43%) - LeetSync

--- 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.cpp
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        priority_queue<long,vector<long>,greater<long>>pq(nums.begin(),nums.end());
+        priority_queue<long,vector<long>,greater<long>>pq(nums.begin(),nums.end());  // heapify logn
 
         int count =0;
 
-        while(!pq.empty() && pq.top() < k){
+// tc  - O(N * 3logn) = O(NlogN)
+// sc - O(N) for heap
+        while(!pq.empty() && pq.top() < k){   // O(N)
 
-            long smallest = pq.top();
-            pq.pop();
+            long smallest = pq.top();  // O(1)
+            pq.pop();  // O(logn)
 
-            long secondsmallest  = pq.top();
-            pq.pop();
+            long secondsmallest  = pq.top(); // O(1)
+            pq.pop(); //O(logn)
 
-            pq.push(min(smallest, secondsmallest) * 2 + max(smallest, secondsmallest));
+            pq.push(min(smallest, secondsmallest) * 2 + max(smallest, secondsmallest));   //O(logn)
 
             count++;
         }
