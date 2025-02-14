3b98b73e49f17b1131718ed654dd43feac2b4547
Pallab Banerjee <91554568+pallab17@users.noreply.github.com>
2025-02-15 02:37:00 +0530

    Time: 17 ms (77.25%) | Memory: 78 MB (55.03%) - LeetSync

--- 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
@@ -1,3 +1,44 @@
+// tc = O(k)
+
+// class ProductOfNumbers {
+// public:
+// vector<int>nums;
+// int n;
+
+//     ProductOfNumbers() {
+//         nums.clear();
+
+//         n=0;
+//     }
+    
+//     void add(int num) {
+//         nums.push_back(num);
+        
+//     }
+    
+//     int getProduct(int k) {
+//         int n = nums.size();
+//         int p=1;
+
+//         for(int i=n-k;i<n;i++){
+//             p*=nums[i];
+//         }
+
+//         return p;
+        
+//     }
+// };
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers* obj = new ProductOfNumbers();
+ * obj->add(num);
+ * int param_2 = obj->getProduct(k);
+ */
+
+
+//  follow up approach
+
 class ProductOfNumbers {
 public:
 vector<int>nums;
@@ -10,26 +51,30 @@ int n;
     }
     
     void add(int num) {
-        nums.push_back(num);
+        if(num==0){
+            nums ={};
+            n=0;
+        }else{
+            if(nums.empty()){
+                nums.push_back(num);
+            }else{
+        nums.push_back(nums[n-1]*num);  //last mal r sathe multiply kore de
+
+            }
+            n++;
+        }
+
         
     }
     
     int getProduct(int k) {
-        int n = nums.size();
-        int p=1;
+       if(k>n){
+        return 0;
+       }else if(k==n){
+        return nums[n-1];
+       }
 
-        for(int i=n-k;i<n;i++){
-            p*=nums[i];
-        }
-
-        return p;
+       return nums[n-1]/nums[n-k-1];
         
     }
 };
-
-/**
- * Your ProductOfNumbers object will be instantiated and called as such:
- * ProductOfNumbers* obj = new ProductOfNumbers();
- * obj->add(num);
- * int param_2 = obj->getProduct(k);
- */
